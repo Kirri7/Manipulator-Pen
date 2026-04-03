714325ae690e46489043e37a117456ce91bf6d4d
Kirri7 <>
2026-04-03 20:41:30 +0300

    rewrite logic with class and methods

--- esp32-connect/esp32-connect/esp32-connect.ino
+++ esp32-connect/esp32-connect/esp32-connect.ino
@@ -1,86 +1,156 @@
-// ESP32 open server on port 10000 to receive a flaot
+// ESP32 Wi-Fi Receiver Module
 
 #include <Arduino.h>
 #include <WiFi.h>
 #define LED_BUILTIN 2
+#define serverPort 10000
+#define ledPin 2
 
-// Replace with your network credentials
 const char* ssid = "xxx";
 const char* password = "xxx";
 
 
-WiFiServer server(10000);  // server port to listen on
+struct ReceivedPhoneData {
+    bool left_signal;
+    bool right_signal;
+    bool up_signal;
+    bool down_signal;
+};
+
+class WiFiReceiver {
+private:
+    WiFiServer server;
+    bool log_to_serial;
+    bool connected;
+    int16_t seqExpected;
+
+public:
+    WiFiReceiver(const bool& do_logging) : connected(false), seqExpected(0), log_to_serial(do_logging), server(serverPort) {}
+    
+    void begin() {
+        printlnSerial("Connecting to " + String(ssid));
+        printlnSerial("Attempting to connect to WiFi network SSID " + String(ssid) + " password " + String(password));
+        
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            printSerial(".");
+            delay(500);
+        }
+        
+        server.begin();
+        printWifiStatus();
+        printlnSerial("Listening on port " + String(serverPort));
+        digitalWrite(ledPin, HIGH);
+        delay(20);
+        digitalWrite(ledPin, LOW);
+    }
+    
+    ReceivedPhoneData receive() {
+        if (!WiFi.isConnected()) {
+            connected = false;
+            digitalWrite(ledPin, LOW);
+            printlnSerial("WiFiReceiver not initialized. Call begin() first.");
+            return {false, false, false, false};
+        }
+        
+        static WiFiClient client;
+
+        if (!client) {
+            connected = false;
+            digitalWrite(LED_BUILTIN, LOW);
+            client = server.available(); // Check for incoming clients
+        }
+        if (client) {
+            if (!connected) {
+                client.flush();
+                connected = true;
+                digitalWrite(ledPin, HIGH);
+                printlnSerial("Client connected!");
+            }
+
+            int length = client.available();
+            if (length > 0) {
+                if (length == sizeof(int32_t)) {
+                    int32_t value;
+                    client.readBytes((char*)&value, sizeof(value));
+                    ReceivedPhoneData data;
+                    data.left_signal = (value & (1 << 0)) != 0;
+                    data.right_signal = (value & (1 << 8)) != 0;
+                    data.up_signal = (value & (1 << 16)) != 0;
+                    data.down_signal = (value & (1 << 24)) != 0;
+                    printlnSerial(
+                        "value " + String(value) +
+                        ",left " + String(data.left_signal) +
+                        ",right " + String(data.right_signal) +
+                        ",up " + String(data.up_signal) +
+                        ",down " + String(data.down_signal)
+                    );
+                    return data;
+                } else {
+                    printSerial("Received invalid packet length: ");
+                    while (client.available()) printSerial(String((char)client.read()));
+                    printlnSerial("");
+                }
+            }
+            else {
+                // Пустые пакеты при выходе из приложения или в перерыве между отправкой данных
+            }
+        }
+        return {false, false, false, false}; // no valid data received -> no movement
+    }
+    
+    void printWifiStatus() {
+        if (!log_to_serial) return;
+
+        Serial.print("\nSSID: ");
+        Serial.println(WiFi.SSID());
+        
+        IPAddress ip = WiFi.localIP();
+        Serial.print("IP Address: ");
+        Serial.println(ip);
+        
+        long rssi = WiFi.RSSI();
+        Serial.print("signal strength (RSSI):");
+        Serial.print(rssi);
+        Serial.println(" dBm");
+    }
+
+    size_t printlnSerial(const String& message) {
+        if (!log_to_serial) return 0;
+        return Serial.println(message);
+    }
+
+    size_t printSerial(const String& message) {
+        if (!log_to_serial) return 0;
+        return Serial.print(message);
+    }
+};
+
+
+WiFiReceiver receiver(true);
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(ledPin, OUTPUT);
     Serial.begin(2000000);
-    // setup Wi-Fi network with SSID and password
-    Serial.printf("Connecting to %s\n", ssid);
-    Serial.printf("\nattempting to connect to WiFi network SSID '%s' password '%s' \n", ssid, password);
-    // attempt to connect to Wifi network:
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print('.');
-        delay(500);
-    }
-    server.begin();
-    // you're connected now, so print out the status:
-    digitalWrite(LED_BUILTIN, HIGH);
-    printWifiStatus();
-    Serial.println(" listening on port 10000");
-}
 
-boolean alreadyConnected = false;  // whether or not the client was connected previously
+    receiver.begin();
+}
 
 void loop() {
-    static WiFiClient client;
-    static int16_t seqExpected = 0;
-    if (!client) {
-        client = server.available();  // Listen for incoming clients
-        digitalWrite(LED_BUILTIN, LOW);
-    }
-    if (client) {                   // if client connected
-        if (!alreadyConnected) {
-            // clead out the input buffer:
-            client.flush();
-            Serial.println("We have a new client");
-            alreadyConnected = true;
-            digitalWrite(LED_BUILTIN, HIGH);
-        }
-        // if data available from client read and display it
-        int length;
-        int32_t value;
-        if ((length = client.available()) > 0) {
-            //str = client.readStringUntil('\n');  // read entire response
-            Serial.printf("Received length %d - ", length);
-            // if data is correct length read and display it
-            if (length == sizeof(value)) {
-                client.readBytes((char*)&value, sizeof(value));
-                bool left = (value & (1 << 0)) != 0;
-                bool right = (value & (1 << 8)) != 0;
-                bool up = (value & (1 << 16)) != 0;
-                bool down = (value & (1 << 24)) != 0;
-                Serial.printf("value %d, left %d, right %d, up %d, down %d \n", value, left, right, up, down);
-            } else
-            while (client.available()) Serial.print((char)client.read());  // discard corrupt packet
-        }
-    }
-}
-
-
-void printWifiStatus() {
-    // print the SSID of the network you're attached to:
-    Serial.print("\nSSID: ");
-    Serial.println(WiFi.SSID());
+    ReceivedPhoneData data = receiver.receive();
     
-    // print your WiFi shield's IP address:
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
+    if (data.left_signal) {
+        Serial.println("Left button pressed");
+    }
+    if (data.right_signal) {
+        Serial.println("Right button pressed");
+    }
+    if (data.up_signal) {
+        Serial.println("Up button pressed");
+    }
+    if (data.down_signal) {
+        Serial.println("Down button pressed");
+    }
     
-    // print the received signal strength:
-    long rssi = WiFi.RSSI();
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");
+    delay(200);
 }
